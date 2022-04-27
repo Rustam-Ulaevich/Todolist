9119a6594d645c906abe7b095b101cc558c56823
Rustam-Ulaevich <praporglamur@gmail.com>
2022-04-27 11:51:04 +0500

    Using tasks.test.ts

--- src/store/tasks.test.ts
+++ src/store/tasks.test.ts
@@ -1,4 +1,4 @@
-import {ActionType, div, mult, numberReducer, sub, sum} from "./tasks";
+import {ActionType, div, multi, numberReducer, sub, sum} from "./tasks";
 
 test('sum of two numbers', () => {
      const a = 2;
@@ -10,7 +10,7 @@ test('subtract of two numbers', () => {
      expect(sub(6, 5)).toBe(1)
 })
 test('multiply of two numbers', () => {
-     expect(mult(5, 5)).toBe(25)
+     expect(multi(5, 5)).toBe(25)
 })
 test('division of two numbers', () => {
      expect(div(100, 2)).toBe(50)
@@ -26,3 +26,65 @@ test('sum with numberReducer', ()=> {
 
      expect(result).toBe(1300)
 })
+test('sub with numberReducer', ()=>{
+     const  salary = 1300
+     const action: ActionType = {
+          type: 'SUB',
+          num: 50
+     }
+     const result = numberReducer(salary, action)
+     expect(result).toBe(1250)
+})
+
+test('multyply with numberReducer', ()=> {
+     const salary = 1250
+     const acti: ActionType = {
+          type: "MULTI",
+          num: 2
+     }
+     const result = numberReducer(salary, acti)
+     expect(result).toBe(2500)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+test('div with numberReducer', ()=> {
+     const salary = 2500
+     const red: ActionType = {
+          type: 'DIV',
+          num: 0.9
+     }
+
+     const result = numberReducer(salary, red)
+
+     expect(result).toBe(2000)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -1,10 +1,10 @@
 export const sum = (a: number, b: number) => a + b;
 export const sub = (a: number, b: number) => a - b;
-export const mult = (a: number, b: number) => a * b;
+export const multi = (a: number, b: number) => a * b;
 export const div = (a: number, b: number) => a / b;
 
 export type ActionType = {
-    type: 'SUM' | 'SUB' | 'MULT' | 'DIV'
+    type: 'SUM' | 'SUB' | 'MULTI' | 'DIV'
     num: number
 }
 
@@ -12,6 +12,12 @@ export const numberReducer = (state: number, action: ActionType ) => {
     switch (action.type) {
         case "SUM":
             return state + action.num
+        case "SUB":
+            return state - action.num
+        case "MULTI":
+            return state * action.num
+        case "DIV":
+            return state / action.num
         default:
             return state
     }
